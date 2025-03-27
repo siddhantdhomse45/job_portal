f05e1966a539f8c0285848f2bea5065cfd3fbe14
siddhantdhomse45 <siddh654@gmail.com>
2025-03-27 12:24:58 -0400

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+import React from "react";
+import { jobsData } from "../assets/assets";
+
+export const AppContext = createContext()
+
+export const AppContextProvider = (props) => {
+
+    const [ searchFilter, setSearchFilter] = useState({
+        title: '',
+        location: ''
+    })
+
+    const [isSearched, setIsSearched] = useState(false)
+
+    const [jobs, setJobs] = useState([])
+
+    //function to fetch jobs
+
+    const fetchJobs = async () => {
+        setJobs(jobsData)
+    }
+
+    useEffect(() => {
+        fetchJobs()
+    }, [])
+
+
+    const value = {
+        setSearchFilter,searchFilter,
+        isSearched, setIsSearched,
+        jobs, setJobs
+    }
+
+    return (<AppContext.Provider value={value}>
+        {props.children}
+        </AppContext.Provider >)
+}
